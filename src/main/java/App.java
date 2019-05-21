2dc0aea0ea83bf06b45fc3a680dfd0ced610a33b
xiadijun <540045123@qq.com>
2019-05-21 22:29:00 +0800

    2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * created by Jay on 2019/5/21
  */
@@ -5,6 +7,8 @@ public class App
 {
     public static void main(String[] args)
     {
-
+        HashMap<String, String> map = new HashMap();
+        System.out.println(map.put("name", "baby"));
+        map.get("name");
     }
 }
